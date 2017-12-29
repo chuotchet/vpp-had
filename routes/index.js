fbf0d75af64e784112fea38bf867cd8c127f10fc
Lai Trong Dao_51200682 <51200682@stu.hcmut.edu.vn>
2017-12-29 23:54:12 +0700

    aaa

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/items', function(req, res, next) {
+  res.render('items');
 });
 
 module.exports = router;
